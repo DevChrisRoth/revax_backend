489c9330c14be5f612eb4ebbfd7abc049debdc88
Christoph Rothmann (CEO) <chrisrothbog@gmail.com>
2022-01-22 16:45:37 +0100

    feat: set methods in trycatch

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -41,91 +41,111 @@ export class AppController {
   })
   @Post('register') //✅
   async register(@Request() req: any): Promise<any> {
-    const UserPersonalData: UserData = {
-      firstname: req.body['firstname'],
-      lastname: req.body['lastname'],
-      birthday: req.body['birthday'],
-      phonenumber: req.body['phonenumber'],
-      description: req.body['description'],
-      jobcategory: req.body['jobcategory'],
-      image1: req.body['image1'] ? req.body['image1'] : null,
-      image2: req.body['image2'] ? req.body['image2'] : null,
-      image3: req.body['image3'] ? req.body['image3'] : null,
-      image4: req.body['image4'] ? req.body['image4'] : null,
-      image5: req.body['image5'] ? req.body['image5'] : null,
-      plz: req.body['plz'],
-      place: req.body['place'],
-      companyname: req.body['companyname'] ? req.body['companyname'] : null,
-      website: req.body['website'] ? req.body['website'] : null,
-    };
-    const UserLoginData: UserLogin = {
-      email: req.body['email'],
-      password: req.body['password'],
-      type: req.body['type'] ? req.body['type'] : 0,
-    };
-    return await this.appService.createUser(UserPersonalData, UserLoginData);
+    try {
+      const UserPersonalData: UserData = {
+        firstname: req.body['firstname'],
+        lastname: req.body['lastname'],
+        birthday: req.body['birthday'],
+        phonenumber: req.body['phonenumber'],
+        description: req.body['description'],
+        jobcategory: req.body['jobcategory'],
+        image1: req.body['image1'] ? req.body['image1'] : null,
+        image2: req.body['image2'] ? req.body['image2'] : null,
+        image3: req.body['image3'] ? req.body['image3'] : null,
+        image4: req.body['image4'] ? req.body['image4'] : null,
+        image5: req.body['image5'] ? req.body['image5'] : null,
+        plz: req.body['plz'],
+        place: req.body['place'],
+        companyname: req.body['companyname'] ? req.body['companyname'] : null,
+        website: req.body['website'] ? req.body['website'] : null,
+      };
+      const UserLoginData: UserLogin = {
+        email: req.body['email'],
+        password: req.body['password'],
+        type: req.body['type'] ? req.body['type'] : 0,
+      };
+      return await this.appService.createUser(UserPersonalData, UserLoginData);
+    } catch (error) {
+      return { status: 'failed' };
+    }
   }
 
   @UseGuards(AuthenticatedGuard)
   @Post('updateprofile') //✅
   async updatePersonalData(@Request() req: any): Promise<any> {
-    const UserData = {
-      firstname: req.body['firstname'],
-      lastname: req.body['lastname'],
-      birthday: req.body['birthday'],
-      phonenumber: req.body['phonenumber'],
-      description: req.body['description'],
-      image1: req.body['image1'] ? req.body['image1'] : null,
-      image2: req.body['image2'] ? req.body['image2'] : null,
-      image3: req.body['image3'] ? req.body['image3'] : null,
-      image4: req.body['image4'] ? req.body['image4'] : null,
-      image5: req.body['image5'] ? req.body['image5'] : null,
-      plz: req.body['plz'],
-      place: req.body['place'],
-      companyname: req.body['companyname'] ? req.body['companyname'] : null,
-      website: req.body['website'] ? req.body['website'] : null,
-    };
-    const password = req.body['password'] ? req.body['password'] : null;
-    return await this.appService.updateUser(
-      UserData,
-      req.user.userid,
-      password,
-    );
+    try {
+      const UserData = {
+        firstname: req.body['firstname'],
+        lastname: req.body['lastname'],
+        birthday: req.body['birthday'],
+        phonenumber: req.body['phonenumber'],
+        description: req.body['description'],
+        image1: req.body['image1'] ? req.body['image1'] : null,
+        image2: req.body['image2'] ? req.body['image2'] : null,
+        image3: req.body['image3'] ? req.body['image3'] : null,
+        image4: req.body['image4'] ? req.body['image4'] : null,
+        image5: req.body['image5'] ? req.body['image5'] : null,
+        plz: req.body['plz'],
+        place: req.body['place'],
+        companyname: req.body['companyname'] ? req.body['companyname'] : null,
+        website: req.body['website'] ? req.body['website'] : null,
+      };
+      const password = req.body['password'] ? req.body['password'] : null;
+      return await this.appService.updateUser(
+        UserData,
+        req.user.userid,
+        password,
+      );
+    } catch (error) {
+      return { status: 'failed' };
+    }
   }
 
   @UseGuards(AuthenticatedGuard)
   @Get('jobcard') //✅
   async getRandomCard(@Request() req: any): Promise<any> {
-    return await this.appService.getRandomJobcard(req.user.userid);
+    try {
+      return await this.appService.getRandomJobcard(req.user.userid);
+    } catch (error) {
+      return { status: 'failed' };
+    }
   }
 
   @UseGuards(AuthenticatedGuard)
   @Post('jobcard') //✅
   async createJobcard(@Request() req: any): Promise<any> {
-    const jobcard = {
-      jobtitle: req.body['title'],
-      description: req.body['description'],
-      jobcategory: req.body['category'],
-      jobtype: req.body['type'],
-      userid_fk: req.user.userid,
-    };
-    return await this.appService.createJobcard(jobcard);
+    try {
+      const jobcard = {
+        jobtitle: req.body['title'],
+        description: req.body['description'],
+        jobcategory: req.body['category'],
+        jobtype: req.body['type'],
+        userid_fk: req.user.userid,
+      };
+      return await this.appService.createJobcard(jobcard);
+    } catch (error) {
+      return { status: 'failed' };
+    }
   }
 
   @UseGuards(AuthenticatedGuard)
   @Post('recommendation') //✅
   async getRecommendation(@Request() req: any): Promise<any> {
-    if (Number(req.body['recommendation']) == 0) {
-      return null;
-    } else {
-      return await this.appService.evalRecommendation(
-        //logged in user
-        req.user.userid,
-        // userid of the clicked item
-        req.body['cardid'],
-        //usertype of the logged in user
-        req.user.type,
-      );
+    try {
+      if (Number(req.body['recommendation']) == 0) {
+        return null;
+      } else {
+        return await this.appService.evalRecommendation(
+          //logged in user
+          req.user.userid,
+          // userid of the clicked item
+          req.body['cardid'],
+          //usertype of the logged in user
+          req.user.type,
+        );
+      }
+    } catch (error) {
+      return { status: 'failed' };
     }
   }
 }
