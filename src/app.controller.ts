c9b752cc48d1b1a1117a52e6793fd0576f6d7209
Christoph Rothmann (CEO) <chrisrothbog@gmail.com>
2022-04-08 08:07:49 +0200

    test: logtail controller for testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Logtail } from '@logtail/node';
+import { Controller, Get, Res } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  logtail = new Logtail('xxMK2pXiUhkTZdja89E2nGYd');
+
+  @Get('logtail')
+  testForLogtail(@Res() res) {
+    this.logtail.info('API Request for Logtail');
+    return res.status(200).json({ message: 'API Request for' });
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,11 +4,11 @@ import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class AppService {
   constructor() {}
-  logtail = new Logtail('xxMK2pXiUhkTZdja89E2nGYd');
+  // logtail = new Logtail('xxMK2pXiUhkTZdja89E2nGYd');
 
-  @Cron('0 * 4 * * *')
-  handleCron() {
-    console.log('Cron job executed');
-    this.logtail.info('Cron job executed');
-  }
+  // @Cron('0 * 4 * * *')
+  // handleCron() {
+  //   console.log('Cron job executed');
+  //   this.logtail.info('Cron job executed');
+  // }
 }
